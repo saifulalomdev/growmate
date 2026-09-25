edb64c9b92e98b36d11af729ed3c0b05d8d398bd
Saiful Alom <hello@saifulalom.com>
2026-09-25 14:47:58 +0600

    refactor(navigation): reorder tabs layout screens
    - Swap position of shops and products screens in the main tab bar navigation.

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ export default function Layout() {
                 options={tabOptions}
             />
             <Tabs.Screen
-                name='shops'
+                name='products'
                 options={tabOptions}
             />
             <Tabs.Screen
@@ -17,7 +17,7 @@ export default function Layout() {
                 options={tabOptions}
             />
             <Tabs.Screen
-                name='products'
+                name='shops'
                 options={tabOptions}
             />
             <Tabs.Screen
